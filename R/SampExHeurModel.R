794fb47de3f899f0a7f32500a6723e3a53b8f6f5
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2018-01-17 22:34:03 +0100

    Many updates to SampExHeurModel.R and SampEx_Fit.R to make things a bit more readible and expandable

--- R/SampExHeurModel.R
+++ R/SampExHeurModel.R
@@ -1,161 +1,238 @@
-### Sample Extrapolation Heuristic Model
 
+# ---------------------
+# Helper functions
+# ---------------------
 
-get_samples <- function(samp, N_samp){
+get_samples <- function(samp,        # samp: History of observations
+                         memory_N) {  # memory_N: Memory
   
-  if (length(samp) > N_samp){
-    new_samp <- samp[length(samp) - N_samp : length(samp)]
+  if (length(samp) > memory_N){
+    new_samp <- samp[length(samp) - memory_N : length(samp)]
   } else if (length(samp) > 0){
-    new_samp <- sample(samp, N_samp, replace = TRUE)
+    new_samp <- sample(samp, memory_N, replace = TRUE)
   } else {
-    new_samp <- round(rnorm(N_samp, 0, 1))
+    new_samp <- round(rnorm(memory_N, 0, 1))
   }
   
 }
 
-SampEx <- function(N, samples_A, samples_B, max_T, t, G, Yt){
+
+# ---------------------
+# Choice rules
+# ---------------------
+
+# Softmax Choice Rule
+#  version = 'log'
+#  version = 'yb': From Yechiam and Busemeyer (2005)
+
+softmax_Choice <- function(impressions,       # impressions: Vector (or list) of impressions of options
+                           phi,               # phi: Choice sensitivity
+                           trial_now,             # trial_now: Current trial
+                           version = "log") { # version: Either 'yb' for Yechiam Busemeyer, or 'log' for log version     
   
-  # N: the memory capacity. This number of observations is considered
-  # samples_A / samples_B: samples drawn so far from each option
-  # max_T: maximum trial number
-  # t: current trial number
-  # G: goal
-  # Yt: number of points earned by trial t
+  # Cleanup inputs
+  if(class(impressions) == "list") {impressions <- unlist(impressions)}
   
-  # get rid of NAs
-  samples_A <- samples_A[!is.na(samples_A)]
-  samples_B <- samples_B[!is.na(samples_B)]
+  # Log version of choice rule
+  if (version == "log") {
+    
+    p_select <- exp(log(trial_now ^ phi) * impressions) / sum(exp(log(trial_now ^ phi) * impressions))
+    
+  }
+  
+  # Yechiam and Busemeyer version
+  if (version == "yb") {
+    
+    p_select <- exp((trial_now / 10) ^ phi * impressions) / sum(exp((trial_now / 10) ^ phi * impressions))
+
+  }
+
+  # Vector of selection probabilities
+  return(p_select)
+  
+}
+
+
+# ---------------------
+# Impression rules
+# ---------------------
+
+# Sample Exploration impression rule
+SampEx_Imp <- function(memory_N,      # N: the memory capacity. This number of observations is considered
+                       selection_v,   # selection_v: Sequential selections made
+                       outcome_v,     # outcome_v: Sequential outcomes observed
+                       trial_max,     # trial_max: Maximum number of trials
+                       trial_now,     # trial_now: Current trial
+                       points_goal,   # points_goal: Points desired at goal. If Infinite, then impressions is based on mean
+                       points_now){   # points_now: Points at current trial
+  
+  # Points needed
+  points_need <- points_goal - points_now
   
   # get Recent Distribution
-  RD_A <- get_samples(samples_A, N)
-  RD_B <- get_samples(samples_B, N)
+  RD_A <- get_samples(outcome_v[selection_v == 1], memory_N)
+  RD_B <- get_samples(outcome_v[selection_v == 2], memory_N)
+  
+  # If goal is finite, impressions are based on likelihood of reaching goal
+  if(is.infinite(points_goal) == FALSE) {
   
   # number of remaining trials
-  R <- max_T - t
+  t_rem <- trial_max - trial_now
   
   # create Recent Extrapolated Distribution
-  ReD_A <- RD_A * R
-  ReD_B <- RD_B * R
+  ReD_A <- RD_A * t_rem
+  ReD_B <- RD_B * t_rem
   
   # for each ReD value, check whether goal is reached or not (ReD binary)
-  ReD_bin_A <- ReD_A > (G - Yt)
-  ReD_bin_B <- ReD_B > (G - Yt)
+  ReD_bin_A <- ReD_A > points_need
+  ReD_bin_B <- ReD_B > points_need
   
-  # calculate likelyhood of reaching the goal from ReD
+  # calculate likelihood of reaching the goal from ReD
   pi_A <- mean(ReD_bin_A)
   pi_B <- mean(ReD_bin_B)
   
-  pis <- list(pi_A, pi_B)
+  # If values are the same, go with mean of ReD
+  if(pi_A == pi_B) {
+    
+    impressions <- c(mean(RD_A), mean(RD_B))
+    
+  } else {
+    
+  # Impressions are likelihood of reaching goal from each option
+  impressions <- c(pi_A, pi_B)
   
+  }
+  
+  }
+  
+  # If goal is infinite, then just go with mean of RDs
+  if(is.infinite(points_goal) == TRUE) {
+    
+    # Impressions are likelihood of reaching goal from each option
+    impressions <- c(mean(RD_A), mean(RD_B))
+    
+  }
+
+  # Return vector of impressions
+  return(impressions)
   
 }
 
-SampEx_noGoal <- function(N, samples_A, samples_B, max_T, t, G, Yt){
-  
-  # N: the memory capacity. This number of observations is considered
-  # samples_A / samples_B: samples drawn so far from each option
-  # max_T: maximum trial number
-  # t: current trial number
-  # G: goal
-  # Yt: number of points earned by trial t
-  
-  # get rid of NAs
-  samples_A <- samples_A[!is.na(samples_A)]
-  samples_B <- samples_B[!is.na(samples_B)]
-  
-  # get Recent Distribution
-  RD_A <- get_samples(samples_A, N)
-  RD_B <- get_samples(samples_B, N)
-  
-  # calculate the mean of the samples
-  m_RD_A <- mean(RD_A)
-  m_RD_B <- mean(RD_B)
-  
-  ms_list <- list(m_RD_A, m_RD_B)
-  
-  
-}
 
-SampEx_noGoalFit <- function(Q, chd, v_As, v_Bs, T, curr_t, goal,
-                             curr_Y, pred = FALSE){
+# ---------------------
+# Model Likelihood rules
+# ---------------------
+
+# Sample Extrapolation Likelihood
+# Given a set of N and phi parameters, calculates the
+#  likelihood of data
+
+SampEx_Lik <- function(pars,             # Parameter vector [N, phi]
+                       selection_v,      # selection_v: Selection vector
+                       outcome_v,        # outcome_v: Outcome vector
+                       trial_v,          # trial_v: Vector of trial numbers
+                       game_v,           # game_v: Vector of game numbers
+                       trial_max = NULL, # trial_max: Maximum number of trials in task
+                       points_goal,      # points_goal: Points desired at goal. If Infinite, then impressions is based on mean
+                       option_n = NULL,  # option_n: Number of options
+                       game_n = NULL     # game_n: Number of games
+                       ){ 
   
-  N_ret <- round(Q[1])
-  phi_s <- Q[2]
+  # Fix missing values
   
-  mean_As <- vector("numeric", length = length(chd))
-  mean_Bs <- vector("numeric", length = length(chd))
+  if(is.null(option_n)) {option_n <- max(selection_v)}
+  if(is.null(game_n)) {game_n <- max(game_v)}
+  if(is.null(trial_max)) {trial_max <- max(trial_v)}
   
-  for (kk in seq_along(chd)){
+  # Get memory_N and phi parameter values
+  memory_N <- round(pars[1])
+  phi <- pars[2]
+  
+  
+  # Extract some information
+  observations_n <- length(selection_v)
+  
+  
+  
+  # Placeholder for selection probabilities for all options
+  lik_mtx <- data.frame(trial = trial_v,
+                         game = game_v,
+                        selection = selection_v,
+                         lik = NA,
+                         pred = NA)
+  
+  # Placeholders for likelihoods of selecting each option
+  lik_mtx[paste0("o_", 1:option_n, "_lik")] <- NA
+  
+  # Loop over games
+  for(game_i in 1:game_n) {
+  
+  # Look over trials
+  for (trial_i in 1:trial_max) {
+
+    points_now <- sum(outcome_v[game_v == game_i & trial_v < trial_i])
+
     
-    ind <- (floor(kk / 25) * 25 + 1) : kk
+    # Get impressions
+    impressions_i <- SampEx_Imp(memory_N = memory_N, 
+                                selection_v = selection_v[game_v == game_i & trial_v < trial_i],
+                                outcome_v = outcome_v[game_v == game_i & trial_v < trial_i],
+                                trial_max = trial_max,
+                                trial_now = trial_i,
+                                points_goal = points_goal,
+                                points_now = points_now)
     
-    ms <- SampEx_noGoal(N_ret, v_As[ind], v_Bs[ind], T, curr_t[kk], goal, curr_Y[kk])
+    # Get selection probabilities
+    selprob_v <- softmax_Choice(impressions = impressions_i, 
+                                phi = phi, 
+                                trial_now = trial_i)
     
-    mean_As[kk] <- ms[[1]]
-    mean_Bs[kk] <- ms[[2]]
+    
+    # Assign absolute prediction to lik_mtx
+    pred_abs <- (1:option_n)[selprob_v == max(selprob_v)]
+    if(length(pred_abs) > 1) {pred_abs <- sample(pred_abs, size = 1)}
+    lik_mtx$pred[lik_mtx$game == game_i & lik_mtx$trial == trial_i] <- pred_abs
+    
+    # Assign vector of selection probabilities to lik_mtx
+    lik_mtx[lik_mtx$game == game_i & lik_mtx$trial == trial_i, paste0("o_", 1:option_n, "_lik")] <- round(selprob_v, 3)
+    
+    # Get likelihood of selection on current trial
+    selection_i <- selection_v[trial_v == trial_i & game_v == game_i]
+    selprob_i <- round(selprob_v[selection_i], 3)
+    
+    # Assign to selprob_mtx
+    lik_mtx$lik[lik_mtx$trial == trial_i & lik_mtx$game == game_i] <- selprob_i
+    
+  }
     
   }
   
-  eee <- rep(NA, length(mean_As))
+  # Get likelihood vector for observed selections
+  lik_v <- lik_mtx$lik
   
-  trial_seq <- rep(1:25, 10)
+  # Censor at extreme values
+  lik_mtx$lik[lik_mtx$lik > .999] <- .999
+  lik_mtx$lik[lik_mtx$lik < .001] <- .001
   
-  eee <- chru(mean_As, mean_Bs, phi_s, trial_seq)
+  # Calculate deviance
+  deviance <- -2 * sum(log(lik_mtx$lik))
   
-  eee[eee > .99999] <- .99999
-  eee[eee < .00001] <- .00001
+  # Calculate G2
+  g2 <- deviance + 2 * length(pars) * log(observations_n)
   
-  if(pred==TRUE) return((round(eee) == chd))
+  # Define final output
   
-  Gsq <- 2*sum(chd[chd!=0]*(log(chd[chd!=0])-log(eee[chd!=0])))
+  output <- list(lik_mtx = lik_mtx,
+                 deviance = deviance,
+                 g2 = g2,
+                 pars = pars)
   
-  return(Gsq)
+  return(output)
   
 }
+           
+          
 
-
-
-SampExFit <- function(Q, chd, v_As, v_Bs, T, curr_t, goal, curr_Y, pred = FALSE){
-  
-  N_ret <- round(Q[1])
-  phi_s <- Q[2]
-
-  pi_As <- vector("numeric", length = length(chd))
-  pi_Bs <- vector("numeric", length = length(chd))
-
-  for (kk in seq_along(chd)){
-
-  	ind <- (floor(kk / 25) * 25 + 1) : kk
-
-  	ps <- SampEx(N_ret, v_As[ind], v_Bs[ind], T, curr_t[kk], goal, curr_Y[kk])
-
-  	pi_As[kk] <- ps[[1]]
-  	pi_Bs[kk] <- ps[[2]]
-
-  }
-
-  eee <- rep(NA, length(pi_As))
-  
-  trial_seq <- rep(1:25, 10)
-
-  eee <- chru(pi_As, pi_Bs, phi_s, trial_seq)
-
-  eee[eee > .99999] <- .99999
-  eee[eee < .00001] <- .00001
-
-  if(pred==TRUE) return((round(eee) == chd))
-
-  Gsq <- 2 * sum(chd[chd!=0] * (log(chd[chd!=0]) - log(eee[chd!=0])))
-  
-  return(Gsq)
-
-}
-
-
-# Logistic Choice Rule
-chru <- function(a,b,phi, tr){
-  1/(1 + exp(-(log10(tr)**phi)*(a - b)))
-} 
 
 
 
--- R/SampEx_Fit.R
+++ R/SampEx_Fit.R
@@ -1,81 +1,205 @@
+
+# Clear working directory
 rm(list = ls())
 gc()
-# SampExHeur Model fitting
 
+# Load libraries
+library(tidyverse)
+library(parallel)
+
+# SampExHeur Model fitting
 source("R/SampExHeurModel.R")
 
-# get data
 
-dat <- readRDS("data/Study1Data/useData/S1_dataTrialLevel.rds")
+# ---------------------------
+# Get data
+# ----------------------------
 
-dat$game <- dat$game - 1
-dat$selection <- as.numeric(as.character(dat$selection))
-dat$selection <- dat$selection - 1
+# get trial level data
+dat <- as_tibble(readRDS("data/Study1Data/useData/S1_dataTrialLevel.rds"))
 
-dat <- subset(dat, game > 0)
+# Data cleaning
+dat <- dat %>%
+  mutate(
+    game = game - 1,
+    selection = as.numeric(as.character(selection))
+  ) %>%
+  filter(
+    game > 0   # Remove game == 0 (practice games I think)
+    ) 
 
-N_sub <- length(unique(dat$id))
+# Number of Subjects
+subjects_N <- length(unique(dat$id))
 
-N_trial <- 25
+# ---------------------------
+# Set up MLE fitting
+# ----------------------------
 
-Goal <- 100
+# Game level parameters
+trial_max <- 25 # Number of trials
+Goal <- 100 # Goal
 
-low_SampEx <- c(1, 0.0001)
-up_SampEx  <- c(15, Inf)
-r_parr_SampEx <- matrix(NA, ncol = 2, nrow = N_sub)
-r_good_SampEx <- c()
-r_pred_SampEx <- c()
+# Grid search parameters
+#   The grid search will look over all combinations of these parameters
+N_par_v <- 1:15
+phi_par_v <- seq(0.0, .15, .01)
+models_to_fit <- c("SampExGoal", "SampExNoGoal", "Random")
 
+subj_max <- 50  # length(unique(dat$id))
 
+# subj_fits contains all participants and models to be fit
+subj_fits <- expand.grid(id = unique(dat$id)[1:subj_max],   # Reduce the number of participants for testing
+                         model = models_to_fit, 
+                         stringsAsFactors = FALSE)
 
-N_par <- 1:20
-phi_par <- seq(0.0, 25, .5)
-
-startp_SampEx <- as.matrix(expand.grid(N_par, phi_par))
-
-nrow(startp_SampEx)
-
-for (ii in seq_len(N_sub)){
+# mle_grid_fun
+#   Takes a row number from subj_fits, and returns MLE estimates for the
+#    subject and model in the given row
+mle_grid_cluster_fun <- function(i) {
   
-  temp_fit_SampEx <- Inf
+  # Get participant id
+  id_i <- subj_fits$id[i]
   
+  # Get subject data
+  dat_subj <- dat %>% 
+    filter(id == id_i)
+  
+  # Get model
+  model_i <- subj_fits$model[i]
+  
+  if(model_i != "Random") {
+  
+  # Define goal based on model
+  points_goal_i <- ifelse(model_i == "SampExGoal", 
+                          100, 
+                          Inf)
+  
+  # Set up grid search
+  par_grid <- expand.grid(N = N_par_v,
+                          phi = phi_par_v)
+  
+  # Loop over par_grid
+  for(par_i in 1:nrow(par_grid)) {
     
-  fit_ind <- dat$id == unique(dat$id)[ii]
-  v_As_temp <- ifelse(dat$selection[fit_ind] == 0, dat$outcome[fit_ind], NA)
-  v_Bs_temp <- ifelse(dat$selection[fit_ind] == 1, dat$outcome[fit_ind], NA)
+    # Get parameters for current run
+    N_i <- par_grid$N[par_i]
+    phi_i <- par_grid$phi[par_i]
     
+    # Fitting values for SampExGoal and SampExNoGoal models
+    if(model_i %in% c("SampExGoal", "SampExNoGoal")) {
+      
+    fits_i <- SampEx_Lik(pars = c(N_i, phi_i),
+                         selection_v = dat_subj$selection + 1,    
+                         outcome_v = dat_subj$outcome,
+                         trial_v = dat_subj$trial,          # trial_v: Vector of trial numbers
+                         game_v = dat_subj$game,           # game_v: Vector of game numbers
+                         trial_max = trial_max,        # trial_max: Maximum number of trials in task
+                         points_goal = points_goal_i,      # points_goal: Points desired at goal. If Infinite, then impressions is based on mean
+                         option_n = 2, # option_n: Number of options
+                         game_n = 10)
     
-  grid_SampEx <- apply(startp_SampEx, 1, SampExFit, chd = dat$selection[fit_ind],
-                      v_As = v_As_temp, v_Bs = v_Bs_temp, T = N_trial,
-                      curr_t = dat$trial[fit_ind], goal = Goal,
-                      curr_Y = dat$points.cum[fit_ind])
+    }
     
+    # Assign g2 to par_grid
+    par_grid$dev[par_i] <- fits_i$deviance
+    par_grid$g2[par_i] <- fits_i$g2
     
+  }
   
+  # Get minimum g2 value and parameters
   
-  r_good_SampEx[ii] <- SampExFit(startp_SampEx[which.min(grid_SampEx),],
-                                 chd = dat$selection[fit_ind],
-                                 v_As = v_As_temp, v_Bs = v_Bs_temp,
-                                 T = N_trial, curr_t = dat$trial[fit_ind],
-                                 goal = Goal, curr_Y = dat$points.cum[fit_ind])
-  r_parr_SampEx[ii,] <- startp_SampEx[which.min(grid_SampEx),]
+  par_grid_min <- par_grid %>% 
+    filter(g2 == min(g2)) %>%
+    sample_n(1) # If there are more than 1 best combinations, choose one at random
   
-  r_pred_SampEx[ii] <- mean(SampExFit(startp_SampEx[which.min(grid_SampEx),],
-                                      chd = dat$selection[fit_ind],
-                                      v_As = v_As_temp, v_Bs = v_Bs_temp,
-                                      T = N_trial, curr_t = dat$trial[fit_ind],
-                                      goal = Goal, curr_Y = dat$points.cum[fit_ind],
-                                      pred=TRUE))
+  }
   
-  print(ii)
+  if(model_i == "Random") {
+    
+    # Set g2 for a random model (and NA paramter values)
+    par_grid_min = data.frame(g2 = -2 * sum(log(rep(.5, trial_max * 10))),
+                              N_mle = NA,
+                              phi_mle = NA)
+  }
+  
+  # Return best values
+  return(c(id = id_i, 
+           model = model_i, 
+           g2 = round(par_grid_min$g2, 3),
+           N_mle = par_grid_min$N,
+           phi_mle =  par_grid_min$phi))
   
 }
 
-med_par_SampEx <- apply(r_parr_SampEx, 2, median)
+# ---------------------------
+# Set up and run cluster
+# ----------------------------
 
-hist(r_pred_SampEx)
+# Set up cluster
+cores_n <- 7   # Number of cores to run on
+cl <- makeCluster(cores_n)
 
-save.image("data/Study1Data/useData/SampExHeur_Fits.RData")
+# Send libraries to cluster
+clusterEvalQ(cl, library(tidyverse))
+
+# Export objects to cluster
+clusterExport(cl, list("subj_fits", "N_par_v", "phi_par_v", 
+                       "trial_max", "Goal", "dat", "SampEx_Lik", 
+                       "SampEx_Imp", "get_samples", "softmax_Choice"))
+
+# Run cluster!
+cluster_result_ls <- parallel::parLapply(cl = cl,
+                                         fun = mle_grid_cluster_fun,
+                                         X = 1:nrow(subj_fits))
+
+stopCluster(cl)  # Stop cluster
+# ---------------------------
+# Organise cluster results
+# ----------------------------
+
+# Convert to dataframe
+cluster_result_df <- as_tibble(do.call(what = rbind, 
+                                       args = cluster_result_ls))
+
+# Convert numeric values
+cluster_result_df <- cluster_result_df %>%
+  mutate(
+    g2 = as.numeric(g2),
+    N_mle = as.numeric(N_mle),
+    phi_mle = as.numeric(phi_mle)
+  )
+
+
+# Combine subj_fits with cluster_result_df
+subj_fits <- subj_fits %>%
+  left_join(cluster_result_df)
+
+# Get the best model for each subject
+
+model_best <- subj_fits %>%
+  group_by(id) %>%
+  summarise(
+    N_models = sum(g2 == min(g2)),       # Number of best fitting models (hopefully 1)
+    model_best = model[g2 == min(g2)][1],
+    model_best_g2 = g2[g2 == min(g2)][1],
+    model_best_N = N_mle[g2 == min(g2)][1],
+    model_best_phi = phi_mle[g2 == min(g2)][1]
+  )
+
+# Combine actual participant conditions with best model
+
+model_best <- dat %>%
+  group_by(id) %>%
+  summarise(
+    goal.condition = goal.condition[1],
+    condition = condition[1]
+  ) %>% 
+  ungroup() %>%
+  left_join(model_best)   # Add modelling results
+
+
+# Table of results
+table(model_best$model_best, model_best$goal.condition)
 
 
 
@@ -88,7 +212,7 @@ save.image("data/Study1Data/useData/SampExHeur_Fits.RData")
 # fitruns <- 2
 # Nstart <- 400
 # 
-# for (ii in seq_len(N_sub)){
+# for (ii in seq_len(subjects_N)){
 # 
 #   temp_fit_SampEx <- Inf
 # 
